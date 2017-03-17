49059aa06068e79aca949752e3e5ccfadb53fd9e
Mattias Hjalmarsson <maze.89@hotmail.com>
2017-03-17 12:03:29 +0100

    Intitial commit

---
+++ LinkDib/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LinkDib.Startup))]
+namespace LinkDib
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
